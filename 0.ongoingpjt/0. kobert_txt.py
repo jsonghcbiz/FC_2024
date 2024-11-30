124af7e7b960d5d7df1ead305a603c8492debbe1
jsonghcbiz <jsong.hcbiz@gmail.com>
2024-11-30 02:46:32 +0000

    update

--- 0.ongoingpjt/0. kobert_txt.py
+++ 0.ongoingpjt/0. kobert_txt.py
@@ -356,7 +356,7 @@ def slack_notification(evaluation_result, model_path, data_path, sample_size, pr
 * MLFlow 정보 *
 - 실행 경로: {mlflow.get_artifact_uri()}
     """
-    slack_webhook_url = 'https://hooks.slack.com/services/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'
+    slack_webhook_url = '**'
     payload = {
         'color': '#00FF00',
         'text': message,
