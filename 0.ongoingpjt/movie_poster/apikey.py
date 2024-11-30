446f402b855165f39c8f7c731ba9aedc1eb3b34b
jsonghcbiz <jsong.hcbiz@gmail.com>
2024-11-30 02:47:56 +0000

    update

--- 0.ongoingpjt/movie_poster/apikey.py
+++ 0.ongoingpjt/movie_poster/apikey.py
@@ -1,2 +1,2 @@
-apikey = 'org-IVd132dKH8iZEpkWfOXDSMJ9'
-slack_webhook_url = 'https://hooks.slack.com/services/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'
+apikey = '**'
+slack_webhook_url = '**'
